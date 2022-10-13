bd7744ca246101bcd51438dbd38eab5faca1386d
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-13 19:24:02 -0400

    use dotenv

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# lab-sort
+![logo_udemy](https://www.udemy.com/staticx/udemy/images/v7/logo-udemy.svg)
+
+# LAB | SORT
+
+This lab was made for the [Typescript: The Complete Developer's Guide](https://www.udemy.com/course/typescript-the-complete-developers-guide/) online course at [Udemy](https://www.udemy.com/).
+
+This code can sort numbers or strings in crescent order using TypeScript and Abstract Class.
+
+## Technologies
+
+- JavaScript
+- Node.js
+- TypeScript
+
+## Set your enviroment variables with
+
+```shell
+NUMBERS_COLLECTION=     // Array of numbers => example: [5, 4, 9]
+CHARACTERS_COLLECTION=  // string => example: usdjsfd
+LINKED_LIST=            // Array of numbers => example: [55, 212, -5]
+```
+
+## Setup
+
+- Fork this repo
+- Clone this repo
+
+```shell
+$ cd lab-sort
+$ npm install
+$ npm run build
+$ npm start
+```
+
+### Available Scripts
+
+In the project directory, you can run:
+
+#### `npm run build`
+
+Transform the TypeScript code into JavaScript in the "build" folder for running.
+
+#### `npm run dev`
+
+Runs the app in the development mode.
+
+The console will reload when you make changes and you can see any lint errors in it.
+
+#### `npm start`
+
+Runs the JavaScript code in the build folder
+
+#### `npm run dev:build`
+
+Transform the TypeScript code into JavaScript in the “build” folder for running and update the "build" folder everytime there is a change in the "src" folder.
+
+#### `npm run dev:run`
+
+Runs the "build" folder in the development mode.
+
+Any changes in the "build" folder will reload the console and you can see any lint errors in it.
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,38 @@
+import "dotenv/config";
+
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-console.log(numbersCollection.data);
-numbersCollection.sort();
-console.log(numbersCollection.data);
+const numbers = process.env.NUMBERS_COLLECTION;
+if (numbers) {
+    console.log('NumbersCollection before sorting => ', numbers);
+    const numbersArray = numbers.trim().slice(1, -1).split(',').map(number => Number(number));
+    const numbersCollection = new NumbersCollection(numbersArray);
+    numbersCollection.sort();
+    console.log('NumbersCollection after sorting => ', numbersCollection.data);
+    console.log();
+};
 
-const charactersCollection = new CharactersCollection('ecbaD');
-console.log(charactersCollection.data);
-charactersCollection.sort();
-console.log(charactersCollection.data);
+const characters = process.env.CHARACTERS_COLLECTION;
+if (characters) {
+    console.log('CharactersCollection before sorting => ', characters);
+    const charactersCollection = new CharactersCollection(characters);
+    charactersCollection.sort();
+    console.log('CharactersCollection after sorting => ', charactersCollection.data);
+    console.log();
+};
 
-const linkedList = new LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
-linkedList.print();
-console.log();
-linkedList.sort();
-linkedList.print();
+const list = process.env.LINKED_LIST;
+if (list) {
+    const linkedList = new LinkedList();
+    const listArray = list.trim().slice(1, -1).split(',').map(number => Number(number));
+    listArray.forEach(number => linkedList.add(number));
+
+    console.log('LinkedList before sorting:');
+    linkedList.print();
+
+    linkedList.sort();
+    console.log('LinkedList after sorting:');
+    linkedList.print();
+};
