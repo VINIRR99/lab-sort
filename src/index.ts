fb0449d71d59af6997c98b5df7b22f61e14fd427
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-13 15:06:52 -0400

    LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 //import { NumbersCollection } from "./NumbersCollection";
-import { CharactersCollection } from "./CharactersCollection";
+//import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 //const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -7,7 +8,18 @@ import { Sorter } from "./Sorter";
 //sortNumbers.sort();
 //console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('ecbaD');
-const sortCharacters = new Sorter(charactersCollection);
-sortCharacters.sort();
-console.log(charactersCollection.data);
+//const charactersCollection = new CharactersCollection('ecbaD');
+//const sortCharacters = new Sorter(charactersCollection);
+//sortCharacters.sort();
+//console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.print();
+console.log();
+const sortLinkedList = new Sorter(linkedList);
+sortLinkedList.sort();
+linkedList.print();
